b77445a30d3f5b74f2723f3527cfe63db5b61f10
Diego Borges <drborges.cic@gmail.com>
2014-09-27 18:08:27 -0300

    creates project structure, and initial sketch of the directives (public/index.html)

---
+++ app.js
@@ -0,0 +1,35 @@
+var path = require('path')
+  , express = require('express')
+  , SocketIO = require('socket.io')
+  , TweetStream = require('node-tweet-stream');
+
+var app = express()
+  , staticContentPath = path.join(__dirname, 'public');
+
+app.use('/static', express.static(staticContentPath))
+app.engine('html', require('ejs').renderFile);
+app.set('views', staticContentPath)
+
+app.get('/', function(req, res){
+  res.render('index.html');
+});
+
+var server = app.listen(8080)
+  , io = SocketIO(server)
+  , tw = TweetStream(require('./config.json'));
+
+io.on('connection', function (socket) {
+  socket.on('track', function (hashtags) {
+    console.log('Tracking the following hashtags:', hashtags);
+    tw.track(hashtags);
+  });
+
+  socket.on('untrack', function (hashtags) {
+    tw.untrack(hashtags);
+  });
+
+  tw.on('tweet', function(tweet) {
+    console.log('new tweet!')
+    socket.emit('tweet', tweet);
+  });
+});
